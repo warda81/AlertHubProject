cf9041c16e9aedc1bb071135f4551914502fd886
warda81 <31844655+warda81@users.noreply.github.com>
2024-10-31 22:03:26 +0200

    first sms commit

---
+++ src/main/java/com/mst/SmsMsApplication.java
@@ -0,0 +1,13 @@
+package com.mst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mst/SmsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
